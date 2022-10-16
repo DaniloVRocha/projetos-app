90cae52f21f52d52b9ee7d58aadd792f444954ef
Danilo Vieira <danilorocha722@gmail.com>
2022-10-15 22:43:48 -0300

    Adicionando POST

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,61 +1,115 @@
-import React from "react";
-import { StyleSheet, TextInput, View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import api from '../services/api';
 import Button from '../components/Button';
 import Header from '../components/Header';
 
 
 
+
 export default function RegisterScreen({ navigation }) {
-    const [text, onChangeText] = React.useState(null);
+
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [valor, setValor] = useState(0);
+    const [acoes, setAcoes] = useState('');
+    const [referencias, setReferencias] = useState('');
+
+    function enviar() {
+        const data = {
+            nome,
+            descricao,
+            valor,
+            acoes,
+            referencias
+        }
+
+        console.log(data);
+
+        api.post("/projetos", data)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
     return (
         <View style={styles.container}>
             <Header style={styles.header} name="CADASTRAR"></Header>
             <TextInput
                 style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
+                onChangeText={setNome}
+                value={nome}
                 placeholder="Nome"
                 keyboardType="text"
             />
             <TextInput
                 style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
+                onChangeText={setDescricao}
+                value={descricao}
                 placeholder="Descrição"
                 keyboardType="text"
             />
             <TextInput
                 style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
+                onChangeText={setValor}
+                value={valor}
                 placeholder="Valor"
-                keyboardType="text"
+                keyboardType="number"
             />
-            <Text style={{margin: 12, fontSize: 15, fontWeight: "bold"}}>Ações</Text>
+            <Text style={{ margin: 12, fontSize: 15, fontWeight: "bold" }}>Ações</Text>
             <TextInput
                 style={styles.textArea}
+                value={acoes}
+                onChangeText={setAcoes}
                 underlineColorAndroid="transparent"
                 placeholder="Insira uma referência por linha"
                 placeholderTextColor="grey"
                 numberOfLines={10}
                 multiline={true}
             />
-            <Text style={{margin: 12, fontSize: 15, fontWeight: "bold"}}>Referências</Text>
+            <Text style={{ margin: 12, fontSize: 15, fontWeight: "bold" }}>Referências</Text>
             <TextInput
                 style={styles.textArea}
+                value={referencias}
+                onChangeText={setReferencias}
                 underlineColorAndroid="transparent"
                 placeholder="Insira uma referência por linha"
                 placeholderTextColor="grey"
                 numberOfLines={10}
                 multiline={true}
             />
-            <Button style={styles.buttonPlus} icon='check'
-                nav='Home' navigation={navigation}></Button>
+            <TouchableOpacity style={[styles.butoa, styles.buttonPlus]}
+                onPress={() => {
+                    enviar();
+                    navigation.navigate('Home')
+                }}>
+                <Icon name='check' style={styles.icon}></Icon>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    butoa: {
+        backgroundColor: "#3F51B5",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 28,
+        shadowColor: "#111",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.2,
+        elevation: 2,
+        minWidth: 40,
+        minHeight: 40
+    },
     container: {
         flex: 1
     },
@@ -85,5 +139,10 @@ const styles = StyleSheet.create({
         height: 150,
         justifyContent: "flex-start",
         margin: 12
+    },
+    icon: {
+        color: "#fff",
+        fontSize: 24,
+        alignSelf: "center"
     }
 });
